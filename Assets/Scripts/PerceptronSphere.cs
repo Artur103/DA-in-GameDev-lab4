c23c15c0f0b7fe1c08747a82d8669e0b66816be4
Artur103 <149056221+Artur103@users.noreply.github.com>
2023-10-26 15:27:27 +0500

    first commit

---
+++ Assets/Scripts/PerceptronSphere.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Perceptron))]
+public class PerceptronSphere : MonoBehaviour
+{
+    public Material ZeroMaterial;
+    public Material OneMaterial;
+
+    private Perceptron perceptron;
+
+    private void Start()
+    {
+        perceptron = GetComponent<Perceptron>();
+        GetComponent<MeshRenderer>().material = perceptron.CalcOutput(0, 0) == 0 ? ZeroMaterial : OneMaterial;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshRenderer>().material = perceptron.CalcOutput(0, 1) == 0 ? ZeroMaterial : OneMaterial;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Finish")
+            GetComponent<MeshRenderer>().material = perceptron.CalcOutput(1, 1) == 0 ? ZeroMaterial : OneMaterial;
+    }
+}
